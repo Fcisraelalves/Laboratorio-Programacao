ba4f36458e5501911ff751445705002f5c934dc8
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-08-28 11:01:59 -0300

    feat(lista-1): Solução do exercício 2

---
+++ src/lista-01/q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float real;
+    puts("Insira um número real: ");
+    scanf("%f", &real);
+    printf("Valor real formatado: %.1f\n", real);
+
+    return 0;
+}
