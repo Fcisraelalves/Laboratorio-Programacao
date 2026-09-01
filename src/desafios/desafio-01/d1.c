29951f026253d2743e533aa976bd992e8e477082
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-08-31 22:52:04 -0300

    refactor(desafio-1): Refatoração da solução do desafio 1
    Essa refatoração atende aos requisitos que estavam ausentes na proposta anterior. Além disso, tentei ao máximo tornar o código o menos redundante possível.

--- src/desafios/desafio-01/d1.c
+++ src/desafios/desafio-01/d1.c
@@ -4,31 +4,49 @@
 
 int main() {
     srand(time(NULL));
-    unsigned char mask = 1;
-    unsigned char armario = 0;
-    unsigned char option;
+    unsigned char mask;
+    unsigned char armarios = 0;
+    unsigned char option = 0;
+    unsigned char armario;
 
     while (option != 3) {
-        puts("Insira uma opção: \n[1] Ocupar armário\n[2] Liberar armário\n[3] Sair\n");
+        mask = 1;
+        puts("\nInsira uma opção: \n[1] Ocupar armário aleatório\n[2] Liberar armário\n[3] Sair\nQualquer outro número será ignorado.");
         scanf("%hhu", &option);
-        if (option == 1) {
-            mask = 1;
-            mask = mask << rand() % 9;
-            armario = armario | mask;
 
-        } else if (option == 2) {
-            mask = 254;
-            mask = mask << rand() % 9;
-            armario = armario & mask;
+        switch (option) {
+            case 3:
+                puts("\nEncerrando...");
+                break;
+            case 1:
+                if (armarios == 255) {
+                    puts("\nTodos os armários já estão ocupados!");
+                } else {
+                    do {
+                        mask = 1;
+                        mask = mask << rand() % 8;
+                    } while (((armarios & mask) == mask));
+                    armarios = armarios | mask;
+                }
 
-        } else {
-            puts("Encerrando...");
+                break;
+            case 2:
+                puts("\nInsira o número do armário a ser desocupado (0-7): ");
+                scanf("%hhu", &armario);
+                if (armario <= 7 && armario >= 0) {
+                    mask = mask << (armario);
+                    armarios = armarios & ~mask;
+                }
+                break;
+            default:
+                puts("\nNúmero inválido...");
         }
-
-        printf("%hhu\n", armario);
-
+        mask = 128;
+        printf("\nMapeamento de Armários: %hhu\n", armarios);
+        for (int i = 7; i >= 0; i--) {
+            printf("%i", ((armarios & mask) == mask ? 1 : 0));
+            mask = mask >> 1;
+        }
     }
-    printf("%hhu\n", armario);
-
     return 0;
 }
